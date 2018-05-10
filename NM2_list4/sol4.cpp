ee6985143f7f637bb705c12e782ff4dfbfa1d142
RafalSwat <37617768+RafalSwat@users.noreply.github.com>
2018-05-10 17:26:13 +0200

    Update sol4.cpp

--- NM2_list4/sol4.cpp
+++ NM2_list4/sol4.cpp
@@ -2,12 +2,18 @@
 #include <cmath>
 #include <vector>
 #include <fstream>
-#include "sol4.h"
+#include "sol_4.h"
 
 void exercise1()
 {
+	//----------------------------------------------
+	// Metoda Laxa-Wendroﬀa
+	//----------------------------------------------
+
 	std::ofstream file;
 	file.open("./NM2_list4_ex1.csv", std::ios::out);
+
+	/// dane
 	double u[50][100];
 	double x = 1;
 	double t = 0;
@@ -16,47 +22,60 @@ void exercise1()
 	double alpha = 2;
 	double s = double((alpha*k) / (2 * h));
 
+	/// zerowanie tablicy na wyniki
+	for (int i = 0; i < 50; i++)
+	{
+		for (int j = 0; j < 100; j++)
+		{
+			u[i][j] = 0;
+		}
+	}
+	/// wyliczanie wartosci z warunkow brzegowych
 	for (size_t i = 0; i < 50; i++)
 	{
 		u[i][0] = std::pow(1 - x, 2);
 		x += h;
 	}
+	/// wyliczanie następnych elementów
 	for (size_t i = 1; i < 49; i++)
 	{
 		for (size_t j = 1; j < 99; j++)
 		{
-			///u[i][j] = (2 * std::pow(s, 2) + s) * u[i + 1][j] + (1 - 4 * std::pow(s, 2) * u[i][j] + (2 * std::pow(s, 2) - s) * u[i - 1][j]);
-			u[i][j] = u[i][j - 1] - (k / (2 * h)) * alpha * (u[i + 1][j - 1] - u[i - 1][j - 1]) + (std::pow(k, 2) / (2 * std::pow(h, 2))) * alpha * alpha
-				* (u[i + 1][j - 1] - 2 * u[i][j - 1] + u[i - 1][j - 1]);
-
+			/// algorytm oparty na Kincaid
+			u[i][j] = (2 * std::pow(s, 2) + s) * u[i + 1][j - 1] + (1 - 4 * std::pow(s, 2) * u[i][j - 1] + (2 * std::pow(s, 2) - s) * u[i - 1][j - 1]);
+			/// algorytm z oparty na wiki
+			/*u[i][j] = u[i][j - 1] - (k / (2 * h)) * alpha * (u[i + 1][j - 1] - u[i - 1][j - 1]) + (std::pow(k, 2) / (2 * std::pow(h, 2))) * alpha * alpha
+				* (u[i + 1][j - 1] - 2 * u[i][j - 1] + u[i - 1][j - 1]);*/
 		}
 	}
-	for (size_t i = 0; i < 50; i++)
+	/// printowanie tablicy
+	for (size_t i = 0; i < 10; i++)
 	{
-		for (size_t j = 0; j < 100; j++)
+		for (size_t j = 0; j < 10; j++)
 		{
-			//std::cout << u[i][j] << '\t' ;
-			file << u[i][j] << '\t';
+			std::cout << u[i][j] << '\t' ;
+			//file << u[i][j] << '\t';
 		}
-		//std::cout << std::endl;
-		file << std::endl;
+		std::cout << std::endl;
+		//file << std::endl;
+		//std::cin.get();
 	}
 }
 void exercise2()
 {
 	std::ofstream file;
 	file.open("./NM2_list4_ex2.csv", std::ios::out);
-	
+
 	double m = 6;
 	double h = 0.5;
 	double k = 3;
 	double n = 1;
-	
+
 	size_t size = 100;
 
 	double * v = new double[size];
 	double * w = new double[size];
-	
+
 	for (int i = 0; i < size; i++)
 	{
 		w[i] = 0;
@@ -67,27 +86,27 @@ void exercise2()
 	v[1] = -cos(h) / 8;
 	v[2] = 0;
 
-	for (int i = 1; i < m; i++)
+	for (int i = 2; i <= m; i++)
 	{
 		h = h / 2;
 		n = 2 * n + 1;
-		for (int j = 0; j < (n + 1) / 2; j++)
+		for (int j = 0; j <= (n + 1) / 2; j++)
 		{
 			w[2 * j] = v[j];
 		}
-		for (int j = 1; j < (n + 1) / 2; j++)
+		for (int j = 1; j <= (n + 1) / 2; j++)
 		{
 			w[2 * j - 1] = (v[j - 1] + v[j]) / 2;
 		}
-		for (int j = 0; j < n + 1; j++)
+		for (int j = 0; j <= n + 1; j++)
 		{
 			v[j] = w[j];
 		}
-		for (int p = 1; p < k; p++)
+		for (int p = 1; p <= k; p++)
 		{
-			for (int j = 1; j < n; j++)
+			for (int j = 1; j <= n; j++)
 			{
-				v[j] = (v[j - 1] + v[j + 1] - h*h*(cos(j*h))) / 2;
+				v[j] = (v[j - 1] + v[j + 1] - h * h*(cos(j*h))) / 2;
 			}
 		}
 		for (int i = 0; i < sizeof(v); i++)
@@ -97,7 +116,7 @@ void exercise2()
 		}
 		std::cout << "h = " << h << std::endl;
 	}
-	
-	delete [] v;
-	delete [] w;
+
+	delete[] v;
+	delete[] w;
 }
