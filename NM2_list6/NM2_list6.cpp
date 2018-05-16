08ca67a63f7a41438374f71ea667b0d0f6224102
RafalSwat <37617768+RafalSwat@users.noreply.github.com>
2018-05-16 17:29:43 +0200

    Create NM2_list6.cpp

---
+++ NM2_list6/NM2_list6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include "sol_6.h"
+
+int main()
+{
+	golden_ratio();
+	std::cout << std::endl << "-----------------------------------" << std::endl;
+	interpolation();
+
+	system("pause");
+	return 0;
+}
